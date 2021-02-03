5f67f49557f7e10714707df7392b4e83a89bb9ea
Hussein Esmail <hussein.esmail7@gmail.com>
2021-02-03 12:57:18 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,5 @@
+chromedriverPath = "" # Path to your executable chromedriver. It is different for each operating system and browser you use
+loginYorkUsername = "" # Your username to log into Moodle
+loginYorkPassword = "" # Your password to log into Moodle
+loginGoogleUsername = "" # Your email address
+loginGooglePassword = "" # Your email address password used to log in and send emails
